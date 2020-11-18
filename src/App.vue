c1cc6f45ec104a52953c8779ccdfdd348354a242
Zeevas <zevdestroyer@gmail.com>
2020-11-18 14:42:49 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header" >
+      <img src="@/assets/mypic1.jpg"/>
+      <h2 class="greet"><span>
+        Personal information: <br>
+        Zevadin Khairudinov <br>
+       22 years old <br>
+       Simferopol,Crimea <br>
+
+       </span></h2>
+      <h1 class="contacts">
+        <span>
+          Contacts: <br>
+          My Git profile:<a href="https://github.com/Zeevas">Zeevas</a><br>
+          My Vk profile:<a href="https://vk.com/zev_destr">Zevadin Khairudinov </a> <br>
+          Gmail:zevdestroyer@gmail.com <br>
+          Phone number:+79780286344 <br>
+        </span></h1>
+    </div>
+    <div class="body">
+      <div class="row">
+  <div class="column side">
+    <h2>My Objective </h2>
+    <p>To contribute professional skills to achieving your company's goals as a web programmer</p>
+    <h2>Personal qualities:</h2>
+     <br>
+   <li> Dependable   </li>
+   <li>Determined</li>
+   <li>Initiative</li>
+   <li>Versatile</li>
+   <li>Positive;)</li>
+  </div>
+  <div class="column middle">
+    <h2>About</h2>
+    <p>Hi!My name is Zevadin Khairudinov.</p>
+    Education: <br>
+    V.I. Vernadsky Taurida National University <br>
+    A bachelor of Automation and computer integrated technologies <br>
+    A bachelor of An Applied Mathematics <br>
+    I`ve got a few projects using Vue js html and css on my github <a href="https://github.com/Zeevas">Zeevas</a>!
+    Go and check them)
+    I don't have a lot of experience in programming, but i am sure i can handle it:) <br>
+    Language knowledge: <br>
+   <ul>Native Crimeantatar</ul>
+   <ul>Intermediate English</ul>
+   <ul>Fluent Russian</ul>
+   Special skills: <br>
+   <ul>Computer literacy</ul>
+   <ul>Driving licence(category B,C)</ul>
+
+  </div>
+  <div class="column side">
+    <h2>My hobbies</h2>
+    <ul>Calistenics</ul>
+    <ul>Music(guitar,trumpet)</ul>
+    <ul>Cars(tuning,repairing,drifting)</ul>
+  </div>
+</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import '@/assets/main.scss'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
