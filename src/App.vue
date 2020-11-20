a504f58f47532013499b11953af55ba4c237a43b
Zeevas <zevdestroyer@gmail.com>
2020-11-20 14:14:11 +0300

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,83 @@
 <template>
-  <div id="app">
-    <div class="header" >
-      <img src="@/assets/mypic1.jpg"/>
-      <h2 class="greet"><span>
-        Personal information: <br>
-        Zevadin Khairudinov <br>
-       22 years old <br>
-       Simferopol,Crimea <br>
+  <div id="app" >
+      <div class="container">
+            <div class="columnAbout">
+              <div class="mypic">
+              </div>
+              <div class="name">
+                Zevadin Khairudinov,22 y.o. <br>
+                Beginner Web Developer
+              </div>
 
-       </span></h2>
-      <h1 class="contacts">
-        <span>
-          Contacts: <br>
-          My Git profile:<a href="https://github.com/Zeevas">Zeevas</a><br>
-          My Vk profile:<a href="https://vk.com/zev_destr">Zevadin Khairudinov </a> <br>
-          Gmail:zevdestroyer@gmail.com <br>
-          Phone number:+79780286344 <br>
-        </span></h1>
-    </div>
-    <div class="body">
-      <div class="row">
-  <div class="column side">
-    <h2>My Objective </h2>
-    <p>To contribute professional skills to achieving your company's goals as a web programmer</p>
-    <h2>Personal qualities:</h2>
-     <br>
-   <li> Dependable   </li>
-   <li>Determined</li>
-   <li>Initiative</li>
-   <li>Versatile</li>
-   <li>Positive;)</li>
-  </div>
-  <div class="column middle">
-    <h2>About</h2>
-    <p>Hi!My name is Zevadin Khairudinov.</p>
-    Education: <br>
-    V.I. Vernadsky Taurida National University <br>
-    A bachelor of Automation and computer integrated technologies <br>
-    A bachelor of An Applied Mathematics <br>
-    I`ve got a few projects using Vue js html and css on my github <a href="https://github.com/Zeevas">Zeevas</a>!
-    Go and check them)
-    I don't have a lot of experience in programming, but i am sure i can handle it:) <br>
-    Language knowledge: <br>
-   <ul>Native Crimeantatar</ul>
-   <ul>Intermediate English</ul>
-   <ul>Fluent Russian</ul>
-   Special skills: <br>
-   <ul>Computer literacy</ul>
-   <ul>Driving licence(category B,C)</ul>
+              <div class="links">
+                <a href="https://github.com/Zeevas"><img src="@/assets/git.png" class="gitimg" ></a>
+                <a href="https://www.instagram.com/zev4ik69/"><img src="@/assets/inst.png" class="instimg"></a>
+                <a href="https://vk.com/zev_destr"><img src="@/assets/vk.png" class="vkimg"></a>
+              </div>
 
-  </div>
-  <div class="column side">
-    <h2>My hobbies</h2>
-    <ul>Calistenics</ul>
-    <ul>Music(guitar,trumpet)</ul>
-    <ul>Cars(tuning,repairing,drifting)</ul>
-  </div>
-</div>
-    </div>
-  </div>
+              </div>
+              <div class="columnInfo">
+                <div class="label">
+                  About Me
+                </div>
+                <hr class="hr">
+                <div class="aboutText1">
+                  Hi!My name is Zevadin Khairudinov.I am a beginner web developer.<br>
+                  I`ve got a few projects using Vue js html and css on my github <a href="https://github.com/Zeevas">Zeevas</a>!
+                  Go and check them)
+                  I don't have a lot of experience in programming, but i am sure i can handle it:)
+
+                </div>
+                <div class="conInfo">
+                  <div class="education">
+                    <h class="titles">Education</h> <br>
+                    V.I. Vernadsky Taurida National University <br>
+                    A bachelor of Automation and computer integrated technologies <br>
+                    A bachelor of An Applied Mathematics <br>
+                  </div>
+                  <div class="langknow">
+                    <h class="titles">Language knowledge</h> <br>
+                    Native Crimeantatar <br>
+                    Intermediate English <br>
+                    Fluent Russian
+                  </div>
+                  <div class="break-column"></div>
+                  <div class="objective">
+                    <h class="titles">My Objective</h><br>
+                    To contribute professional skills to achieving your company's goals as a web developer.
+                  </div>
+                  <div class="specSkills">
+                    <h class="titles">Special skills</h><br>
+                    Computer literacy<br>
+                    Basic Vue js knowledge<br>
+                    Basic React js knowledge<br>
+                    Html and CSS knowledge<br>
+                  </div>
+                  <div class="persQual">
+                    <h class="titles">Personal qualities</h><br>
+                    Dependable<br>
+                    Determined<br>
+                    Initiative<br>
+                    Versatile<br>
+                    Positive;)
+                  </div>
+                  <div class="hobbies">
+                    <h class="titles">My hobbies</h> <br>
+                    Calistenics <br>
+                    Music(guitar,trumpet) <br>
+                    Cars(tuning,repairing) <br>
+
+                  </div>
+                </div>
+              </div>
+              </div>
+         </div>
+
 </template>
 
 <script>
 import '@/assets/main.scss'
+
 export default {
   name: 'App'
 }
